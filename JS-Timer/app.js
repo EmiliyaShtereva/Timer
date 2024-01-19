a7e7180f1cbcded0a6c95e8ff3e2f19f3c4039d0
EmiliyaShtereva <123276538+EmiliyaShtereva@users.noreply.github.com>
2024-01-19 11:07:10 +0200

    added functionality for time update

--- JS-Timer/app.js
+++ JS-Timer/app.js
@@ -1,19 +1,30 @@
+let conteiner = document.querySelector('.conteiner');
+let formConteiner = document.querySelector('.form');
 let hoursEl = document.getElementById('hours');
 let minutesEl = document.getElementById('minutes');
 let secondsEl = document.getElementById('seconds');
 let startBtn = document.querySelector('.control-btn');
 let timeBtn = document.querySelector('.time-btn');
+let submitBtn = document.querySelector('.submit-btn');
 
 let interval = null;
 let time = 0;
-let hours = Number(hoursEl.innerText) * 3600;
-let minutes = Number(minutesEl.innerText) * 60;
-let seconds = Number(secondsEl.innerText);
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
 
 startBtn.addEventListener('click', startHandler);
-timeBtn.addEventListener('click', newTimeHandler);
+timeBtn.addEventListener('click', () => {
+    conteiner.style.display = 'none';
+    formConteiner.style.display = 'inline';
+    stopHandler();
+});
+submitBtn.addEventListener('click', submitHandler);
 
 function startHandler() {
+    hours = Number(hoursEl.innerText) * 3600;
+    minutes = Number(minutesEl.innerText) * 60;
+    seconds = Number(secondsEl.innerText);
     time = hours + minutes + seconds;
 
     if (interval === null) {
@@ -22,11 +33,11 @@ function startHandler() {
         interval = setInterval(() => {
             time--;
             updateTime();
-    
+
             if (time === 0) stopHandler();
-    
+
         }, 1000);
-    
+
         updateClass();
     } else {
         stopHandler();
@@ -61,6 +72,31 @@ function updateClass() {
     }
 }
 
-function newTimeHandler() {
-    
+function submitHandler(e) {
+    e.preventDefault();
+    let form = e.target.parentElement;
+    let formData = new FormData(form);
+
+    let hours = Number(formData.get('hours'));
+    let minutes = Number(formData.get('minutes'));
+    let seconds = Number(formData.get('seconds'));
+
+    if (hours > 99) {
+        hours = 99;
+    }
+
+    if (minutes >= 60) {
+        minutes = 59;
+    }
+
+    if (seconds >= 60) {
+        seconds = 59
+    }
+
+    hoursEl.innerText = hours.toString().padStart(2, '0');
+    minutesEl.innerText = minutes.toString().padStart(2, '0');
+    secondsEl.innerText = seconds.toString().padStart(2, '0');
+
+    formConteiner.style.display = 'none';
+    conteiner.style.display = 'inline';
 }
